a4ba022d209e9d961ce6dadf5bf9a8a98c76bf4f
kirill <kirisa49@gmail.com>
2025-09-14 23:51:50 +0700

    Deploy React Bio

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      'qnmbp-80-216-186-147.a.free.pinggy.link'
-    ]
-  }
+  base: '/'
 })
